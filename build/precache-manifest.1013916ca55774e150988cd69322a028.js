027e0cf37564bbdcc1e862f3a1e5ad3ccce701c7
guru-schnecke <eiweala@live.com>
2019-11-27 16:13:09 +0300

    ready for deployment

---
+++ build/precache-manifest.1013916ca55774e150988cd69322a028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be8a34a4d2240002ac01483b2c4cf9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "099737558e0e723b4c8a",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8f1b329d543aa1065226",
+    "url": "/static/js/2.5175a669.chunk.js"
+  },
+  {
+    "revision": "099737558e0e723b4c8a",
+    "url": "/static/js/main.1ec1abbd.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
